dfe9c1b70e704b1887f7ba49c7ce137aebd52ab5
Michelle Lin <michelleamazinglin@gmail.com>
2020-11-16 17:58:46 -0500

    set up empty file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "Dont_forget_the_COFFEE_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "do_not_forget_the_coffee_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 

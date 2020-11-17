23b860e5567d5542727acc6f08997e3538337712
Michelle Lin <michelleamazinglin@gmail.com>
2020-11-17 11:10:19 -0500

    backend auth and frontend folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
-
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:create]
-    resource :session, only: [:create, :destroy]
+    resources :benches, only: [:index, :show, :create]
+    resources :reviews, only: [:create]
+    resource :user, only: [:create]
+    resource :session, only: [:create, :destroy, :show]
+    resource :favorites, only: [:create, :destroy]
   end
 
-  root to: 'static_pages#root'
-
+  root "static_pages#root"
 end
--- frontend/reducers/entities_reducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import usersReducer from './users_reducer';
-// import sessionReducer from './session_reducer';
-
-export default combineReducers({
-    users: usersReducer,
-})

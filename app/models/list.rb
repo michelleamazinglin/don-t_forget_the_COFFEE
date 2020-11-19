1ff06d26e44bfc7e5905042dc9b8c729273d6dd9
Michelle Lin <michelleamazinglin@gmail.com>
2020-11-19 18:34:13 -0500

    add lists table

---
+++ app/models/list.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: lists
+#
+#  id         :bigint           not null, primary key
+#  user_id    :integer          not null
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class List < ApplicationRecord
+    
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_230120) do
+ActiveRecord::Schema.define(version: 2020_11_19_225634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lists", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_lists_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name", null: false
